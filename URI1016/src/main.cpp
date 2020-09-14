9315575b7ea3644c3af8b3a8caed719794916f8d
Tomaz Martins <tomaz.r.martins@gmail.com>
2020-09-14 11:42:59 -0300

    URI1016(beginner): distance

---
+++ URI1016/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int const distance_per_hour = 30;
+    int distance = 0;
+
+    std::cin >> distance;
+
+    std::cout << (distance * 2) << " minutos" << std::endl;
+    return 0;
+}
